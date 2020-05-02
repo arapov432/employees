7e3e9c48e03adf7ac3003d041157803737c75630
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-05-01 18:27:20 -0700

    simple pagination

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import './list.css'
 class List extends Component {
-        
+        constructor(props) {
+            super(props);
+            this.state = {
+                currentPage: 1,
+                limit: 15
+                
+            }
+        }
+
+    setPage = (arrow) => {
+        const { currentPage, limit } = this.state;
+        const { employees } = this.props;
+        const rightLimit = Math.ceil(employees.length / limit);
+
+        if(arrow === 'prev' && currentPage > 1) {
+            this.setState(prevState => ({currentPage: prevState.currentPage - 1}))
+        }
+        else if (arrow === 'next' && currentPage < rightLimit) {
+            this.setState(prevState => ({currentPage: prevState.currentPage + 1}))
+        }
+
+    }
     render(){
+        const { currentPage, limit } = this.state;
+
+        console.log("currentPage", currentPage);
+
         const { employees, sortByFn, deleteEmployee } = this.props;
+        // logic
+        const start = (currentPage - 1)*limit
+        const end = currentPage*limit;
+        const paginationData = employees.slice(start, end);
+
         return (
             <div className="list">
                 <div className="row header">
-                    <div onClick={() => sortByFn("id")} className="cell sm">#</div>
                     <div onClick={() => sortByFn("id")} className="cell sm">ID</div>
                     <div onClick={() => sortByFn("first_name")} className="cell">Name</div>
                     <div onClick={() => sortByFn("last_name")} className="cell">Last Name</div>
@@ -18,11 +47,10 @@ class List extends Component {
                 </div>
                 <div className="content">
                     {
-                        employees.map((employee, ind) => {
+                        paginationData.map((employee, ind) => {
                             const { id, first_name, last_name, email, city, state } = employee;
                             return (
                             <div key={id} className="row">
-                                <div className="cell sm">{ind}</div>
                                 <div className="cell sm">{id}</div>
                                 <div className="cell">{first_name}</div>
                                 <div className="cell">{last_name}</div>
@@ -35,6 +63,13 @@ class List extends Component {
                         })
                     }
                 </div>
+                <div className="pagination">
+                    <div onClick={() => this.setPage('prev')} className="prev arrow">Prev</div>
+                    <div className="start arrow">{start} - </div>  
+                    <div className="end arrow">{end}</div>
+                    <div onClick={() => this.setPage('next')} className="next arrow">Next</div>
+                </div>
+
             </div>
         )
     }
